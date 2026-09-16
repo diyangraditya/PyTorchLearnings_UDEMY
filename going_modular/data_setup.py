14b33ffe6b29ca171403a34e3a32757919e6bd3a
Diyang Raditya A <diyangraditya144@gmail.com>
2026-09-16 19:27:12 +0700

    chore: learn modular

---
+++ going_modular/data_setup.py
@@ -0,0 +1,16 @@
+"""
+containes functionality for creating pytorch dataloaders for image classification data
+"""
+import os
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+NUM_WORKERS = os.cpu_count()
+def create_dataloaders(
+    train_dir: str,
+    test_dir: str,
+    transform: transforms.Compose,
+    batch_size: int,
+    num_workers: int=NUM_WORKERS
+):
+ pass
